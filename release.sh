e14aceba0c31756fd262dcdadaacf376322e9365
Andrei Finski <Andrei_Finski@epam.com>
2018-11-19 12:45:41 +0300

    Separated build and release in travis config
    Added .sh script for gradle release to work fine in Travis:
    https://github.com/travis-ci/travis-ci/issues/1701
    Might be removed and changed with more native approach once issue is closed

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Travis + Gradle release"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global push.default current
+git stash
+git remote set-url origin https://$GH_TOKEN@github.com/reportportal/agent-java-spock
+git checkout master
+git update-index --chmod=+x gradlew
+chmod +x gradlew
+./gradlew release -Prelease.useAutomaticVersion=true --debug --stacktrace
