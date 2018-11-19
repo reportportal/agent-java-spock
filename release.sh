3824a7c7207dcca330af9e64e96fc76cf54d9f1d
Andrei Finski <Andrei_Finski@epam.com>
2018-11-19 14:58:08 +0300

    AF repo

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
 git stash
-git remote set-url origin https://$GH_TOKEN@github.com/reportportal/agent-java-spock
+git remote set-url origin https://$GH_TOKEN@github.com/andreifinski/agent-java-spock
 git checkout master
 git update-index --chmod=+x gradlew
 chmod +x gradlew
