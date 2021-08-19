68e56faba585a6b2fd9056e7c87bc670ae468d0e
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-08-19 15:13:34 +0300

    A nested step test

---
+++ src/test/groovy/com/epam/reportportal/spock/nestedsteps/TestNoNestedStepsWhenNoParameters.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.reportportal.spock.nestedsteps;
+
+import com.epam.reportportal.service.ReportPortal;
+import com.epam.reportportal.service.ReportPortalClient;
+import com.epam.reportportal.spock.ReportPortalSpockListener;
+import com.epam.reportportal.spock.features.OrderedInteractionsSpec;
+import com.epam.reportportal.spock.utils.TestExtension;
+import com.epam.reportportal.spock.utils.TestUtils;
+import com.epam.reportportal.util.test.CommonUtils;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.Result;
+
+import static com.epam.reportportal.spock.utils.TestUtils.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.*;
+
+public class TestNoNestedStepsWhenNoParameters {
+	private final String classId = CommonUtils.namedId("class_");
+	private final String methodId = CommonUtils.namedId("method_");
+
+	private final ReportPortalClient client = mock(ReportPortalClient.class);
+
+	@BeforeEach
+	public void setupMock() {
+		TestUtils.mockLaunch(client, null, classId, methodId);
+		TestUtils.mockBatchLogging(client);
+		TestExtension.listener = new ReportPortalSpockListener(ReportPortal.create(client, standardParameters(), testExecutor()));
+	}
+
+	@Test
+	public void verify_no_nested_steps_reported() {
+		Result result = runClasses(OrderedInteractionsSpec.class);
+
+		assertThat(result.getFailureCount(), equalTo(0));
+
+		verify(client).startLaunch(any());
+		verify(client).startTestItem(any(StartTestItemRQ.class));
+		verify(client).startTestItem(same(classId), any(StartTestItemRQ.class));
+
+		verify(client).finishTestItem(eq(methodId), any());
+		verify(client).finishTestItem(eq(classId), any());
+		verifyNoMoreInteractions(client);
+	}
+}
