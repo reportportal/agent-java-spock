2a97b9adb76ec0f77fccd50c9b1c7d3298446173
Pushkar Nayama <pnayama@affinionds.com>
2021-10-31 14:49:50 +0530

    Handled the scenario when data value is null

--- src/main/java/com/epam/reportportal/spock/NodeInfoUtils.java
+++ src/main/java/com/epam/reportportal/spock/NodeInfoUtils.java
@@ -128,7 +128,7 @@ final class NodeInfoUtils {
 		Object[] dataValues = iterationInfo.getDataValues();
 		if (!parameterNames.isEmpty() && dataValues != null) {
 			iterationDescription += "\n" + IntStream.range(0, parameterNames.size())
-					.mapToObj(i -> parameterNames.get(i) + ": " + dataValues[i].toString())
+					.mapToObj(i -> parameterNames.get(i) + ": " + (dataValues[i] == null ? "null" : dataValues[i].toString()))
 					.collect(Collectors.joining("; "));
 		}
 
